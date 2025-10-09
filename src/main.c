5f7f5b1ae4728f92eaf9b3bce567ab0329592c3d
Mathieu Poirier <mathieuworkemail@gmail.com>
2025-10-09 00:12:23 -0400

    Implemented stubs to initialize SysTick hardware timer.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,18 @@
+/* Some calling considerations that we need */
+/* We need to setup: */
+/* - SysTick Management */
+/* - Dynamic Memory */
+/* - Peripherials like UART needed for terminal */
+/* - Task stacks and scheduler structure */
+#include "systick.h"
+
+extern int _sdata, _edata, _sbss, _ebss;
+
 int main(void)
 {
         while (1)
         {
-
+                systick_init();
         }
         return 0;
 }
