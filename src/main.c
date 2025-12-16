0b32fdfd6a28723a4482e057174a651747f492f3
Mathieu Poirier <mathieuworkemail@gmail.com>
2025-12-15 22:45:29 -0500

    mock uart test 1

--- src/main.c
+++ src/main.c
@@ -1,56 +1,36 @@
-/* Some calling considerations that we need */
-/* We need to setup: */
-/* - SysTick Management */
-/* - Dynamic Memory */
-/* - Peripherials like UART needed for terminal */
-/* - Task stacks and scheduler structure */
-/* - Watchdog for safe resetting */
-
-
 #include "../include/stdint.h"
-#include "../include/isr.h"
 
-extern int _sdata, _edata, _sbss, _ebss;
-extern void systick_init(uint32_t ticks);
-extern void hse_clock_init(void);
-extern void switch_to_pll_clock(void);
-extern void pll_init(void);
-extern void pll_enable(void);
-extern void flash_latency_init(void);
-extern void set_bus_prescalers(void);
 extern void enable_gpioa1(void);
 extern void enable_usart1_clock(void);
 extern void pa9_to_tx(void);
+
 extern void usart1_init_tx(unsigned int brr);
 extern void usart1_putc(char c);
 
-static uint32_t last = 0;
+static void delay(volatile uint32_t n)
+{
+        while (n--)
+        {
+                __asm volatile("nop");
+        }
+}
 
 int main(void)
 {
-        // Clock management
-        hse_clock_init();
-        flash_latency_init();
-        set_bus_prescalers();
-        pll_init();
-        pll_enable();
-        switch_to_pll_clock();
-
-        // Enable onboard peripheral clocks
+        // 1) Enable clocks for GPIOA + USART1
         enable_gpioa1();
         enable_usart1_clock();
+
+        // 2) Configure PA9 = AF7 (USART1_TX)
         pa9_to_tx();
 
-        
+        // 3) Init USART1 TX at 115200 assuming PCLK2 = 16 MHz (HSI)
+        usart1_init_tx(139);
 
-        systick_init(215999);
+        // 4) Prove TX works: send a repeating pattern
         while (1)
         {
-                if (g_ticks != last && g_ticks % 1000 == 0)
-                {
-                        last = g_ticks;
-                        // checking for interupts
-                }
+                usart1_putc('U'); // 0x55 pattern is easy to see on a scope too
+                delay(800000);
         }
-        return 0;
 }
